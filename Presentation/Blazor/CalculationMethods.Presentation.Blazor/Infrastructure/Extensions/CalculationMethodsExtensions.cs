17e2c761bc19ed119e4138de824c975172cfed14
Rukin Danil <rukin2018d@yandex.ru>
2022-09-29 12:43:10 +0700

    команды для сохранения и восстановления

---
+++ Presentation/Blazor/CalculationMethods.Presentation.Blazor/Infrastructure/Extensions/CalculationMethodsExtensions.cs
@@ -0,0 +1,54 @@
+﻿using CalculationMethods.Core.Entities;
+using CalculationMethods.Infrastructure.Entities.Double;
+
+namespace CalculationMethods.Presentation.Blazor.Infrastructure.Extensions
+{
+    public static class CalculationMethodsExtensions
+    {
+        public static IVector<double> ChangeSize(this IVector<double> oldVector, int size)
+        {
+            IVector<double> result = new DoubleVector(size);
+            if (size > oldVector.Size)
+            {
+                for (int i = 0; i < oldVector.Size; i++)
+                {
+                    result[i] = oldVector[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    result[i] = oldVector[i];
+                }
+            }
+            return result;
+        }
+
+        public static ISquareMatrix<double> ChangeSize(this ISquareMatrix<double> matrix, int size)
+        {
+            ISquareMatrix<double> result = new DoubleSquareMatrix(size);
+            if (matrix.Size < size)
+            {
+                for (int i = 0; i < matrix.Size; i++)
+                {
+                    for (int j = 0; j < matrix.Size; j++)
+                    {
+                        result[i, j] = matrix[i, j];
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        result[i, j] = matrix[i, j];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
--- Presentation/Blazor/CalculationMethods.Presentation.Blazor/Pages/LUFactorization.razor.cs
+++ Presentation/Blazor/CalculationMethods.Presentation.Blazor/Pages/LUFactorization.razor.cs
@@ -2,7 +2,9 @@
 using CalculationMethods.Core.Services.Dialogs;
 using CalculationMethods.Core.Services.Factories.Base;
 using CalculationMethods.Core.Services.Repositories;
+using CalculationMethods.Infrastructure.Entities.Double;
 using CalculationMethods.Presentation.Blazor.Infrastructure.Commands;
+using CalculationMethods.Presentation.Blazor.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using System.Windows.Input;
@@ -12,7 +14,6 @@ namespace CalculationMethods.Presentation.Blazor.Pages
     public partial class LUFactorization
     {
         private bool _isSolutionVisible = false;
-        private string _path;
         private int _matrixSize = 0;
         private double _norm;
         private double _conditionNumber;
@@ -23,7 +24,7 @@ namespace CalculationMethods.Presentation.Blazor.Pages
         private IMatrix<double> _reversedMatrix;
         private IVector<double> _vectorB;
         private IVector<double> _solutionVector;
-        private InputVariant _selectedInputVariant;
+        private InputVariant _selectedInputVariant = InputVariant.Edit;
         private InputVariant SelectedInputVariant
         {
             get => _selectedInputVariant;
@@ -36,7 +37,7 @@ namespace CalculationMethods.Presentation.Blazor.Pages
                     _editable = true;
             }
         }
-        private bool _editable = false;
+        private bool _editable = true;
 
         [Inject]
         protected IFactory<double> RepositoryFactory { get; set; }
@@ -50,6 +51,9 @@ namespace CalculationMethods.Presentation.Blazor.Pages
             RestoreSystemCommand = new LambdaCommand(OnRestoreSystemCommandExecuted, CanRestoreSystemCommandExecute);
             SaveSystemCommand = new LambdaCommand(OnSaveSystemCommandExecuted, CanSaveSystemCommandExecute);
             AcceptMatrixSizeCommand = new LambdaCommand(OnAcceptMatrixSizeCommandExecuted, CanAcceptMatrixSizeCommandExecute);
+            SaveVectorCommand = new LambdaCommand(OnSaveVectorCommandExecuted, CanSaveVectorCommandExecute);
+            SaveMatrixCommand = new LambdaCommand(OnSaveMatrixCommandExecuted, CanSaveMatrixCommandExecute);
+            SaveSolutionVectorCommand = new LambdaCommand(OnSaveSolutionVectorCommandExecuted, CanSaveSolutionVectorCommandExecute);
         }
 
         #region ClearMatrixCommand
@@ -153,7 +157,12 @@ namespace CalculationMethods.Presentation.Blazor.Pages
         {
             try
             {
-                
+                _matrix = _matrix.ChangeSize(_matrixSize);
+                _vectorB = _vectorB.ChangeSize(_matrixSize);
+                _solutionVector = _solutionVector.ChangeSize(_matrixSize);
+
+                _isSolutionVisible = false;
+
                 StateHasChanged();
             }
             catch (Exception ex)
@@ -164,6 +173,138 @@ namespace CalculationMethods.Presentation.Blazor.Pages
         }
         private bool CanAcceptMatrixSizeCommandExecute(object p) => true;
         #endregion
+
+        #region SaveMatrixCommand
+        public ICommand SaveMatrixCommand { get; }
+        private void OnSaveMatrixCommandExecuted(object p)
+        {
+            try
+            {
+                string _matrixFileName = Path.Combine(Environment.CurrentDirectory, Configuration["matrix"]);               
+                RepositoryFactory.FileRepositoryFactory(_matrixFileName).CreateSquareMatrixRepository().Save(_matrix);
+                snackbar.Add("Система сохранена", MudBlazor.Severity.Info);
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                snackbar.Add(ex.Message, MudBlazor.Severity.Error);
+                StateHasChanged();
+            }
+        }
+        private bool CanSaveMatrixCommandExecute(object p) => true;
+        #endregion
+
+        #region SaveVectorCommand
+        public ICommand SaveVectorCommand { get; }
+        private void OnSaveVectorCommandExecuted(object p)
+        {
+            try
+            {
+                string _vectorFileName = Path.Combine(Environment.CurrentDirectory, Configuration["vector"]);
+                RepositoryFactory.FileRepositoryFactory(_vectorFileName).CreateVectorRepository().Save(_vectorB);
+
+                snackbar.Add("Система сохранена", MudBlazor.Severity.Info);
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                snackbar.Add(ex.Message, MudBlazor.Severity.Error);
+                StateHasChanged();
+            }
+        }
+        private bool CanSaveVectorCommandExecute(object p) => true;
+        #endregion
+
+        #region SaveSolutionVectorCommand
+        public ICommand SaveSolutionVectorCommand { get; }
+        private void OnSaveSolutionVectorCommandExecuted(object p)
+        {
+            try
+            {
+                string _solutionVectorFileName = Path.Combine(Environment.CurrentDirectory, Configuration["solutionVector"]);
+                RepositoryFactory.FileRepositoryFactory(_solutionVectorFileName).CreateVectorRepository().Save(_solutionVector);
+
+                snackbar.Add("Система сохранена", MudBlazor.Severity.Info);
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                snackbar.Add(ex.Message, MudBlazor.Severity.Error);
+                StateHasChanged();
+            }
+        }
+        private bool CanSaveSolutionVectorCommandExecute(object p) => true;
+        #endregion
+
+        #region RestoreMatrixCommand
+        public ICommand RestoreMatrixCommand { get; }
+        private void OnRestoreMatrixCommandExecuted(object p)
+        {
+            try
+            {
+                string _matrixFileName = Path.Combine(Environment.CurrentDirectory, Configuration["matrix"]);
+                _matrix = RepositoryFactory.FileRepositoryFactory(_matrixFileName).CreateSquareMatrixRepository().Get();
+
+                snackbar.Add("Система восстановлена", MudBlazor.Severity.Info);
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                snackbar.Add(ex.Message, MudBlazor.Severity.Error);
+                StateHasChanged();
+            }
+        }
+        private bool CanRestoreMatrixCommandExecute(object p) => true;
+        #endregion
+
+        #region RestoreVectorCommand
+        public ICommand RestoreVectorCommand { get; }
+        private void OnRestoreVectorCommandExecuted(object p)
+        {
+            try
+            {
+                string _vectorFileName = Path.Combine(Environment.CurrentDirectory, Configuration["vector"]);
+                _vectorB = RepositoryFactory.FileRepositoryFactory(_vectorFileName).CreateVectorRepository().Get();
+
+                snackbar.Add("Система восстановлена", MudBlazor.Severity.Info);
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                snackbar.Add(ex.Message, MudBlazor.Severity.Error);
+                StateHasChanged();
+            }
+        }
+        private bool CanRestoreVectorCommandExecute(object p) => true;
+        #endregion
+
+        #region RestoreSolutionVectorCommand
+        public ICommand RestoreSolutionVectorCommand { get; }
+        private void OnRestoreSolutionVectorCommandExecuted(object p)
+        {
+            try
+            {
+                string _solutionVectorFileName = Path.Combine(Environment.CurrentDirectory, Configuration["solutionVector"]);
+                _solutionVector = RepositoryFactory.FileRepositoryFactory(_solutionVectorFileName).CreateVectorRepository().Get();
+
+                snackbar.Add("Система восстановлена", MudBlazor.Severity.Info);
+                StateHasChanged();
+            }
+            catch (Exception ex)
+            {
+                snackbar.Add(ex.Message, MudBlazor.Severity.Error);
+                StateHasChanged();
+            }
+        }
+        private bool CanRestoreSolutionVectorCommandExecute(object p) => true;
+        #endregion
+
+        protected override void OnInitialized()
+        {
+            _matrix = new DoubleSquareMatrix(_matrixSize);
+            _vectorB = new DoubleVector(_matrixSize);
+            _solutionVector = new DoubleVector(_matrixSize);
+        }
     }
 
     public enum InputVariant
